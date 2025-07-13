25688e39b2a1af02cfd53360f20f2b7e2dabd04e
AsRealTech <114587693+AsRealTech@users.noreply.github.com>
2025-07-13 04:47:16 +0100

    Update Logout.jsx

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from "react-router-dom";
 
 const LogoutButton = () => {
+const navigate = useNavigate();
   const handleLogout = () => {
     sessionStorage.removeItem("token");
     // For example, redirect to login page:
